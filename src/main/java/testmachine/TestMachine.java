79abe18a119d9b4f4ee192f8213c9d91096027fa
mgh <m.ghoreshi@gmail.com>
2020-10-07 18:57:31 +0330

    add a test machine sample file

--- src/main/java/testmachine/TestMachine.java
+++ src/main/java/testmachine/TestMachine.java
@@ -14,218 +14,220 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Queue;
 import java.util.Stack;
+
 import sbu.testlab.testmachine.tracer.oracle.IExecuteModel;
 
 public class TestMachine {
 
-public HashMap<String, Arcs> startStateTrans;
-public HashMap<String, Arcs> endStateTrans;
-public List<Arc> allArcs;
-public List<Arc> initArcs;
-public State states;
+    public HashMap<String, Arcs> startStateTrans;
+    public HashMap<String, Arcs> endStateTrans;
+    public List<Arc> allArcs;
+    public List<Arc> initArcs;
+    public State states;
 
-public int curr;
+    public int curr;
 
-public TestMachine(State states, int init) {
-	startStateTrans = new HashMap<String, Arcs>(states.size());
-	endStateTrans = new HashMap<String, Arcs>(states.size());
-	for (String stateId : states.keySet()) {
-		startStateTrans.put(stateId, new Arcs());
-		endStateTrans.put(stateId, new Arcs());
-	}
-	this.states = states;
-	this.allArcs = new ArrayList<Arc>();
-	this.initArcs = new ArrayList<Arc>();
-}
+    public TestMachine(State states, int init) {
+        startStateTrans = new HashMap<String, Arcs>(states.size());
+        endStateTrans = new HashMap<String, Arcs>(states.size());
+        for (String stateId : states.keySet()) {
+            startStateTrans.put(stateId, new Arcs());
+            endStateTrans.put(stateId, new Arcs());
+        }
+        this.states = states;
+        this.allArcs = new ArrayList<Arc>();
+        this.initArcs = new ArrayList<Arc>();
+    }
 
-public void addTransition(int i, int j, String method, boolean isPartial) {
-	// TODO Auto-generated method stub
-addTransition(i + "", j + "", method, isPartial);
-}
+    public void addTransition(int i, int j, String method, boolean isPartial) {
+        // TODO Auto-generated method stub
+        addTransition(i + "", j + "", method, isPartial);
+    }
 
-public void addTransition(String start, String end, String method,
-		boolean isPartial) {
-	Arc arc = new Arc(start, end, method, isPartial);
-	if (!arc.getStart().equals("null")) {
-		startStateTrans.get(start).add(arc);
-		endStateTrans.get(end).add(arc);
-		allArcs.add(arc);
-	} else {
-		initArcs.add(arc);
-	}
-}
+    public void addTransition(String start, String end, String method,
+                              boolean isPartial) {
+        Arc arc = new Arc(start, end, method, isPartial);
+        if (!arc.getStart().equals("null")) {
+            startStateTrans.get(start).add(arc);
+            endStateTrans.get(end).add(arc);
+            allArcs.add(arc);
+        } else {
+            initArcs.add(arc);
+        }
+    }
 
-public TestCases primePaths(IExecuteModel exeModel) {
+    public TestCases primePaths(IExecuteModel exeModel) {
 
-TestCases result = new TestCases();
-Date d1 = new Date();
-long iteration = 0;
-for (Arc initArc : initArcs) {
+        TestCases result = new TestCases();
+        Date d1 = new Date();
+        long iteration = 0;
+        for (Arc initArc : initArcs) {
 
-	Stack<StackNode> stack = new Stack<StackNode>();
+            Stack<StackNode> stack = new Stack<StackNode>();
 
-	stack.push(new StackNode(new Arcs(initArc), startStateTrans
-			.get(initArc.getEnd()), exeModel));
+            stack.push(new StackNode(new Arcs(initArc), startStateTrans
+                    .get(initArc.getEnd()), exeModel));
 
-	stack.peek().getExeModel().invoke(initArc.getMethod());
+            stack.peek().getExeModel().invoke(initArc.getMethod());
 
-	while (!stack.isEmpty()) {
-		if (iteration > 10000)
-			break;
-		Arcs currPath = (Arcs) stack.peek().getPath().clone();
+            while (!stack.isEmpty()) {
+                if (iteration > 10000)
+                    break;
+                Arcs currPath = (Arcs) stack.peek().getPath().clone();
 
-		if (stack.peek().getArcs().isEmpty()) {
-			if (stack.peek().isPrime())
-				result.add(new TestCase(currPath, stack.peek()
-						.getExeModel().toString()));
+                if (stack.peek().getArcs().isEmpty()) {
+                    if (stack.peek().isPrime())
+                        result.add(new TestCase(currPath, stack.peek()
+                                .getExeModel().toString()));
 
-			stack.pop();
-			continue;
-		}
+                    stack.pop();
+                    continue;
+                }
 
-		while (!stack.peek().getArcs().isEmpty()) {
-			Arc arc = stack.peek().getArcs().remove();
-			iteration++;
-			boolean isSatisfy = stack
-					.peek()
-					.getExeModel()
-					.satisfyState(arc.getMethod(),
-							states.get(arc.getEnd()));
-			if (isSatisfy) {
-				if (!currPath.isRepeated(arc)) {
-					stack.peek().setPrime(false);
-					currPath.add(arc);
+                while (!stack.peek().getArcs().isEmpty()) {
+                    Arc arc = stack.peek().getArcs().remove();
+                    iteration++;
+                    boolean isSatisfy = stack
+                            .peek()
+                            .getExeModel()
+                            .satisfyState(arc.getMethod(),
+                                    states.get(arc.getEnd()));
+                    if (isSatisfy) {
+                        if (!currPath.isRepeated(arc)) {
+                            stack.peek().setPrime(false);
+                            currPath.add(arc);
 
-					stack.push(new StackNode(currPath, startStateTrans
-							.get(arc.getEnd()), stack.peek()
-							.getExeModel()));
-					stack.peek().getExeModel().invoke(arc.getMethod());
-					break;
-				}
-			}
-		}
-	}
-	}
-	Date d2 = new Date();
-	long time = d2.getTime() - d1.getTime();
+                            stack.push(new StackNode(currPath, startStateTrans
+                                    .get(arc.getEnd()), stack.peek()
+                                    .getExeModel()));
+                            stack.peek().getExeModel().invoke(arc.getMethod());
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+        Date d2 = new Date();
+        long time = d2.getTime() - d1.getTime();
 
-	System.out.print("======Test Machine Tools======\n==========Statistics==========\n Elapsed Time: "
-		+ time / 1000 + "s");
-System.out.print("\n Number of Comparisons: " + iteration + " \n");
+        System.out.print("======Test Machine Tools======\n==========Statistics==========\n Elapsed Time: "
+                + time / 1000 + "s");
+        System.out.print("\n Number of Comparisons: " + iteration + " \n");
 
-	return result;
-}
+        return result;
+    }
 
-public String ExportToDotString() {
-	StringBuilder s = new StringBuilder();
-	s.append("digraph G {\n");
-for (Arc arc : allArcs) {
-	s.append(String.format("%s -> %s [label=%s];\n", arc.start,
-			arc.end, arc.method));
-}
-s.append("}");
-	return s.toString();
+    public String ExportToDotString() {
+        StringBuilder s = new StringBuilder();
+        s.append("digraph G {\n");
+        for (Arc arc : allArcs) {
+            s.append(String.format("%s -> %s [label=%s];\n", arc.start,
+                    arc.end, arc.method));
+        }
+        s.append("}");
+        return s.toString();
 
-}
+    }
 
-	/**
-	 * extract arcs of the machine from string like this (each line contain one arc):
-	 *   arcsStr="
-	 *       start_state, end_state, arc_name \n
-	 *       start_state, end_state, arc_name \n
-	 * 	     ...
-	 * 	     "
-	 * @param arcsStr
-	 */
-	public void fromString(String arcsStr){
-		String[] lines=arcsStr.trim().split("\n");
-		for(String line:lines){
-			String[] parts =line.trim().split(",");
-			this.addTransition(parts[0].trim(), parts[1].trim(), parts[2].trim(), true);
-		}
-	}
+    /**
+     * extract arcs of the machine from string like this (each line contain one arc):
+     * arcsStr="
+     * start_state, end_state, arc_name \n
+     * start_state, end_state, arc_name \n
+     * ...
+     * "
+     *
+     * @param arcsStr
+     */
+    public void fromString(String arcsStr) {
+        String[] lines = arcsStr.trim().split("\n");
+        for (String line : lines) {
+            String[] parts = line.trim().split(",");
+            this.addTransition(parts[0].trim(), parts[1].trim(), parts[2].trim(), true);
+        }
+    }
 
-public void fromFile(String path) {
-	InputStream fis;
-	BufferedReader br = null;
-	String line;
+    public void fromFile(String path) {
+        InputStream fis;
+        BufferedReader br = null;
+        String line;
 
-	try {
-		fis = new FileInputStream(path);
-		br = new BufferedReader(new InputStreamReader(fis,
-				Charset.forName("UTF-8")));
-	while ((line = br.readLine()) != null) {
-		if (line.trim().equals(""))
-			continue;
-		String[] part = line.trim().split("\t");
+        try {
+            fis = new FileInputStream(path);
+            br = new BufferedReader(new InputStreamReader(fis,
+                    Charset.forName("UTF-8")));
+            while ((line = br.readLine()) != null) {
+                if (line.trim().equals(""))
+                    continue;
+                String[] part = line.trim().split("\t");
 
-		this.addTransition(part[0], part[1], part[2], true);
-	}
-	br.close();
-} catch (FileNotFoundException e) {
-	// TODO Auto-generated catch block
-	e.printStackTrace();
-} catch (IOException e) {
-	// TODO Auto-generated catch block
-		e.printStackTrace();
-	} finally {
+                this.addTransition(part[0], part[1], part[2], true);
+            }
+            br.close();
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } finally {
 
-		br = null;
-		fis = null;
-	}
-}
+            br = null;
+            fis = null;
+        }
+    }
 
-public class StackNode {
+    public class StackNode {
 
-	Arcs path;
-	Queue<Arc> arcs;
-	boolean isPrime;
+        Arcs path;
+        Queue<Arc> arcs;
+        boolean isPrime;
 
-	IExecuteModel exeModel;
+        IExecuteModel exeModel;
 
-	StackNode(Arcs path, List<Arc> arcs, IExecuteModel exeModel) {
-		this.path = path;
-		this.isPrime = true;
-		this.arcs = new ArrayDeque<Arc>();
-		for (Arc arc : arcs)
-			this.arcs.add(arc);
+        StackNode(Arcs path, List<Arc> arcs, IExecuteModel exeModel) {
+            this.path = path;
+            this.isPrime = true;
+            this.arcs = new ArrayDeque<Arc>();
+            for (Arc arc : arcs)
+                this.arcs.add(arc);
 
-		try {
-			this.exeModel = exeModel.getClass().newInstance();
-		} catch (InstantiationException e) {
-			// TODO Auto-generated catch block
-		e.printStackTrace();
-	} catch (IllegalAccessException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
-	}
-	this.exeModel.SetSateAs(exeModel);
-}
+            try {
+                this.exeModel = exeModel.getClass().newInstance();
+            } catch (InstantiationException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            } catch (IllegalAccessException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+            this.exeModel.SetSateAs(exeModel);
+        }
 
-public Arcs getPath() {
-	return path;
-}
+        public Arcs getPath() {
+            return path;
+        }
 
-public Queue<Arc> getArcs() {
-	return arcs;
-}
+        public Queue<Arc> getArcs() {
+            return arcs;
+        }
 
-public boolean isPrime() {
-	return isPrime;
-}
+        public boolean isPrime() {
+            return isPrime;
+        }
 
-public void setPrime(boolean isPrime) {
-	this.isPrime = isPrime;
-}
+        public void setPrime(boolean isPrime) {
+            this.isPrime = isPrime;
+        }
 
-public IExecuteModel getExeModel() {
-	return exeModel;
-}
+        public IExecuteModel getExeModel() {
+            return exeModel;
+        }
 
-@Override
-public String toString() {
-	return "[path:" + path.toString() + " - arcs:" + arcs.toString();
-		}
-	}
+        @Override
+        public String toString() {
+            return "[path:" + path.toString() + " - arcs:" + arcs.toString();
+        }
+    }
 
 }
